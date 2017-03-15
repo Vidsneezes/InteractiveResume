1d9293eff17d7bec3228f366d0a49c90b0a911a2
Vidsneezes <moraleszez@gmail.com>
2017-03-15 10:14:42 -0600

    import page css to page js , modified style

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Page>
+        <Page title="Info">
           <Info />
         </Page>
         <PersonalInfo />
--- src/components/page/Page.jsx
+++ src/components/page/Page.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Page.css';
 
 class Page extends Component {
   render() {
