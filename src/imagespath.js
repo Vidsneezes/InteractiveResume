b73f5c482a0430e77c750178840e877679456df1
Vidsneezes <moraleszez@gmail.com>
2017-03-16 09:09:31 -0600

    added a assets dictionary from images

--- src/imagespath.js
+++ src/imagespath.js
@@ -1,9 +1,14 @@
-import images from './images/';
+import defaultImage from './images/default.png';
+import project_1Image from './images/64x64icon_2.png';
+import project_2Image from './images/64x64icon_3.png';
 
 class assets {
     loadImages(){
-        this.images = [];
-        
+        this.images = {
+            "default":defaultImage,
+            "project1":project_1Image,
+            "project2":project_2Image
+        };
     }
 }
 
